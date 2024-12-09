62cc9e08ace430d2f1d8c2faf266542657131b4e
marcos <m_german4@mail.fhsu.edu>
2024-12-09 15:38:04 +0100

    add sign in and register views

--- soccer_recruitment/soccer_recruitment/urls.py
+++ soccer_recruitment/soccer_recruitment/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from players import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('signin/', views.signin, name='signin'),
 ]
